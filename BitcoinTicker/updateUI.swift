7ae6767ba1b227db47472ab5bcafb933d873c135
Joshua Kades <josh.kades@gmail.com>
2017-12-31 17:07:23 -0700

    Initial commit

---
+++ BitcoinTicker/updateUI.swift
@@ -0,0 +1,30 @@
+//
+//  updateUI.swift
+//  BitcoinTicker
+//
+//  Created by Francine Kades on 9/29/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class updateUI: UIViewController {
+
+    let prices : Double = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+    
+    
+    
+    
+    
+}
